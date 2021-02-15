5ec80db87a7b68884e62e254e7580305f7388e49
cpiette <cpiette@tink.ca>
2021-02-15 16:27:14 -0500

    Start

---
+++ src/main/java/com/chris/app/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.chris.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String getName() {
+        return "test";
+    }
+}
